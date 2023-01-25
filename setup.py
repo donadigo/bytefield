50571e7523ea506cf77d5ee3a40d3dfe612801da
Adam Bieńkowski <donadigos159@gmail.com>
2023-01-25 21:57:05 +0100

    Update setup.py, release 1.0.1

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name="bytefield",
     packages=find_packages(exclude=["tests*"]),
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
     description="Parse binary data using declarative field layout and native Python properties",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="donadigo",
     author_email="donadigo@gmail.com",
     url="https://github.com/donadigo/bytefield",
